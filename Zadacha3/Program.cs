e390a4f4f30334489e91086a02b0679d393246f8
Natalya <nanona@example.com>
2022-12-11 12:51:35 +0700

    Added Zadacha3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить  их произведение.
+
+int m = ReadInt("Введите количество строк матрицы А: ");
+int n = ReadInt("Введите количество столбцов матрицы А: ");
+int[,] matrixA = new int[m, n];
+
+FillMatrixRandomNumbers(matrixA);
+WriteMatrix(matrixA);
+
+Console.WriteLine();
+
+int c = ReadInt("Введите количество строк матрицы Б: ");
+int d = ReadInt("Введите количество столбцов матрицы Б: ");
+int[,] matrixB = new int[c, d];
+
+FillMatrixRandomNumbers(matrixB);
+WriteMatrix(matrixB);
+
+Console.WriteLine();
+
+if (matrixA.GetLength(1) != matrixB.GetLength(0))
+{
+    Console.WriteLine("Умножение невозможно");
+    return;
+}
+
+int[,] matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+
+for (int i = 0; i < matrixA.GetLength(0); i++)
+{
+    for (int j = 0; j < matrixB.GetLength(1); j++)
+    {
+        matrixC[i, j] = 0;
+        for (int k = 0; k < matrixA.GetLength(1); k++)
+        {
+            matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+        }
+    }
+}
+WriteMatrix(matrixC);
+
+
+
+void FillMatrixRandomNumbers(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void WriteMatrix(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+    Console.WriteLine();
+    }
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
